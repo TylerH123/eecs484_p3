67537b73dfbe4e54044d0b50db568ac3606499d9
Tyler <tylhuang@umich.edu>
2022-11-04 14:41:01 -0400

    push

--- query1.js
+++ query1.js
@@ -6,12 +6,17 @@ function find_user(city, dbname) {
     db = db.getSiblingDB(dbname);
 
     var results = [];
+    var temp = {};
+
     // TODO: find all users who live in city
     db.users.find({ "current.city": city }, { "user_id": 1, "_id": 0, "current.city": 1 }).forEach(element => {
         const obj = JSON.parse(JSON.stringify(element));
-        print(JSON.stringify(element));
+        print(obj);
         results.push(obj.user_id);
+
+        temp[obj.user_id] = null;
     });
+    print(temp.keys());
 
 
     // See test.js for a partial correctness check.
