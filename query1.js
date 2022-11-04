c0241c40a955513ce1f8c1d962788ab4761144e4
Tyler <tylhuang@umich.edu>
2022-11-04 14:33:07 -0400

    push

--- query1.js
+++ query1.js
@@ -8,7 +8,7 @@ function find_user(city, dbname) {
     var results = [];
     // TODO: find all users who live in city
     db.users.find({ "current.city": city }).forEach(element => {
-        print(element.name);
+        print(element);
     });
 
 
