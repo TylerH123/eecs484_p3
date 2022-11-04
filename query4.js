62b38e4017349dd808f28016ff63f9ef24edad52
Tyler <tylhuang@umich.edu>
2022-11-04 15:55:10 -0400

    push

--- query4.js
+++ query4.js
@@ -29,7 +29,7 @@ function suggest_friends(year_diff, dbname) {
                 }
             }).forEach((e) => {
                 pairs.push([obj.user_id, e.user_id]);
-                print(obj.hometown.city, e.hometown.city);
+                print(obj.gender, e.gender);
             });
         }
     });;
