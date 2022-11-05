89448b9fbf125768ddf2b5d6eaadce3530c9cebf
Tyler <tylhuang@umich.edu>
2022-11-05 13:00:22 -0400

    push

--- query6.js
+++ query6.js
@@ -9,9 +9,10 @@ function find_average_friendcount(dbname) {
     // let sum = 0; 
     // let count = db.users.count(); 
 
-    let avg = db.users.aggregate([{ $group: { "_id": "_id", average: { $avg: { $size: "$friends" } } } }]);
-    print(tojson(avg.next()));
-    return tojson(avg.next()).average;
+    let cursor = db.users.aggregate([{ $group: { "_id": "_id", average: { $avg: { $size: "$friends" } } } }]);
+    let avg = tojson(cursor.next());
+    print(avg);
+    return avg;
 }
 
 
