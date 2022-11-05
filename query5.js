386b692fb0299c749ca35aba39c5b873ad95f343
Tyler <tylhuang@umich.edu>
2022-11-05 16:53:21 -0400

    push

--- query5.js
+++ query5.js
@@ -21,8 +21,9 @@ function oldest_friend(dbname) {
         let oldest = -1;
         let oldestYOB = Number.MAX_VALUE;
         user.friends.forEach(friend => {
+            print(friend);
             let temp = JSON.stringify(db.users.find({ "user_id": friend }));
-            print(temp);
+            // print(temp);
             if (temp.YOB < oldestYOB) {
                 oldest = temp.user_id;
                 oldestYOB = temp.YOB;
